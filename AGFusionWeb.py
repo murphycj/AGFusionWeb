4fe43f151d9055bbc0bee4f46fb180f5d26522c4
murphycj <murphy.charlesj@gmail.com>
2016-09-30 08:06:20 -0400

    enabled basic plotting, connected to db

--- AGFusionWeb.py
+++ AGFusionWeb.py
@@ -1,19 +1,19 @@
 import sqlite3
 import os
 
+import agfusion
+import mpld3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+db = agfusion.AGFusionDB(
+    '/Users/charlesmurphy/Desktop/Research/AGFusion/AGFusion/data/agfusion.db',
+    84,
+    'mouse'
+)
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'vgfusion.db'),
@@ -23,12 +23,47 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-rv = connect_db()
-
 @app.route('/')
 def index():
     session['gallery_count']=0
     return render_template('index.html')
+
+@app.route('/plot',methods=["GET","POST"])
+def plot():
+
+    #gene5prime = agfusion.Gene(
+    #    gene=str(request.form['5primegene']),
+    #    junction=int(request.form['5primeloc']),
+    #    db=db
+    #)
+
+    #gene3prime = agfusion.Gene(
+    #    gene=str(request.form['3primegene']),
+    #    junction=int(request.form['3primeloc']),
+    #    db=db
+    #)
+    gene5prime = agfusion.Gene(
+        gene='ENSMUSG00000022770',
+        junction=31684294,
+        db=db
+    )
+
+    gene3prime = agfusion.Gene(
+        gene='ENSMUSG00000002413',
+        junction=39648486,
+        db=db
+    )
+
+    fusion = agfusion.Fusion(
+        gene5prime=gene5prime,
+        gene3prime=gene3prime,
+        db=db
+    )
+
+    dict_of_plots=fusion.output_to_html()
+
+
+    return render_template('plot.html', dict_of_plots=dict_of_plots)
 
 @app.route('/gallery')
 def gallery():
@@ -45,7 +80,6 @@ def help():
 @app.route('/feedback')
 def feedback():
     return render_template('feedback.html')
-
 
 @app.route('/thanks')
 def thanks():
